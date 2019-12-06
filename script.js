fc2519f7a0c77cdb6e7ef59c786735a1e5bfb6b7
DavidOLaosun <54552473+DavidOLaosun@users.noreply.github.com>
2019-12-06 17:54:15 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,77 @@
+/******************************************
+Treehouse FSJS Techdegree:
+project 1 - A Random Quote Generator
+******************************************/
+
+
+var quotes = [
+
+	{
+		'quote': 'We have pain,but not as great as his name',
+		'source': 'Jamie Grace',
+		'citation': 'AZ Quotes',
+		'tag': 'Gospel Singers',
+	},
+
+	{
+		'quote': 'Focusing on the negative only makes a difficult journey more difficult.',
+		'source': 'TobyMac',
+		'citation': 'AZ Quotes',
+		'tag': 'Gospel Singers',
+	},
+
+	{
+		'quote': 'You lead me and keep me from falling.You carry me close to Your heart.And surely Your goodness and mercy will follow me.',
+		'source': 'Chris Tomlin',
+		'citation': 'AZ Quotes',
+		'tag': 'Gospel Singers',
+	},
+
+	{
+		'quote': 'I will walk by faith, even when I cannot see',
+		'source': 'Jeremy Camp',
+		'citation': 'AZ Quotes',
+		'tag': 'Gospel Singers',
+	},
+
+	{
+		'quote': 'Give me open hands and open doors.',
+		'source': 'Matthew West',
+		'citation': 'AZ Quotes',
+		'tag': 'Gospel Singers',
+	},
+
+	{
+		'quote': 'In the middle of my little mess,I forget how BIG i`m blessed!'
+		'source': 'Francesca Battistelli',
+		'citation': 'AZ Quotes',
+		'tag': 'Gospel Singers',
+	},
+
+	{
+		'quote': 'The area where we struggle the most is often the area of our greatest effectiveness.',
+		'source': 'Mandisa',
+		'citation': 'AZ Quotes',
+		'tag': 'Gospel Singers',
+	},
+];
+
+function getRandomQuote(){
+	var randomNumber;
+	randomNumber = Math.floor(Math.random() * quotes.length);
+	//clear interval
+	//clearInterval(intervalid);
+	return quotes[randomNumber];
+}
+
+function randomBgColor(){
+
+}
+  
+  
+  /***
+	 * click event listener for the print quote button
+	 * DO NOT CHANGE THE CODE BELOW!!
+	 ***/
+
+	document.getElementById('load-quote').addEventListener("click", printQuote, false);
